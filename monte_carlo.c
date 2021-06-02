91acec81d17d2c7012f19e0143f5add9d07e2bb8
Sreekanth-38 <sreekanth_b190078me@nitc.ac.in>
2021-06-02 20:06:13 +0530

    changes made

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,7 +3,22 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
+	x=frandom();
+	y=frandom();
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
+	}
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
+}
 
 float frandom() {
   long int q = random();
